330a07ffe061039909b117d882e15f0c1fda49ca
CodePlay2016 <13269308268@163.com>
2018-12-26 19:38:28 +0800

    change network base channels number from 16 to 32

--- HW3/src/net.py
+++ HW3/src/net.py
@@ -7,21 +7,21 @@ def discriminator(inpt,gn_stddev, training=True):
             gn_stddev: a scalar, the stddev for the gaussian noise added to the input image,
     '''
     with tf.variable_scope('dis',reuse=tf.AUTO_REUSE):
-        fs = 16
+        channels = 32
         inpt = inpt+tf.random_normal(shape=tf.shape(inpt), mean=0.0, stddev=gn_stddev, dtype=tf.float32)
-        out = tf.layers.conv2d(inpt, filters=fs, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(inpt, filters=channels, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
         out = tf.nn.leaky_relu(out)
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
 
-        out = tf.layers.conv2d(out, filters=fs*2, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(out, filters=channels*2, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
         out = tf.nn.leaky_relu(out)
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
 
-        out = tf.layers.conv2d(out, filters=fs*4, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(out, filters=channels*4, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
         out = tf.nn.leaky_relu(out)
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
 
-        out = tf.layers.conv2d(out, filters=fs*8, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(out, filters=channels*8, kernel_size=4, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
         out = tf.nn.leaky_relu(out)
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
 
@@ -30,7 +30,7 @@ def discriminator(inpt,gn_stddev, training=True):
     return out
 
 def generator(inpt,training=True):
-    channels = 16
+    channels = 32
     with tf.variable_scope('gen',reuse=tf.AUTO_REUSE):
         out = tf.layers.dense(inpt, channels*8*16*16)
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
