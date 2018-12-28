1208922ef2139c2266d7ae6a55cb6ef53fc5ddc2
CodePlay2016 <13269308268@163.com>
2018-12-28 19:04:53 +0800

    fix discriminator error

--- HW3/src/net.py
+++ HW3/src/net.py
@@ -9,24 +9,24 @@ def discriminator(inpt,gn_stddev, training=True):
     with tf.variable_scope('dis',reuse=tf.AUTO_REUSE):
         channels = 32
         inpt = inpt+tf.random_normal(shape=tf.shape(inpt), mean=0.0, stddev=gn_stddev, dtype=tf.float32)
-        out = tf.layers.conv2d(inpt, filters=channels, kernel_size=5, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(inpt, filters=channels, kernel_size=5, strides=1, padding='SAME') 
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
         out = tf.nn.leaky_relu(out)
 
-        out = tf.layers.conv2d(out, filters=channels*2, kernel_size=5, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(out, filters=channels*2, kernel_size=5, strides=2, padding='SAME')
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
         out = tf.nn.leaky_relu(out)
 
-        out = tf.layers.conv2d(out, filters=channels*4, kernel_size=5, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(out, filters=channels*4, kernel_size=5, strides=2, padding='SAME')
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
         out = tf.nn.leaky_relu(out)
 
-        out = tf.layers.conv2d(out, filters=channels*8, kernel_size=5, strides=2, padding='SAME',kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.conv2d(out, filters=channels*8, kernel_size=5, strides=2, padding='SAME')
         out = tf.layers.batch_normalization(out,epsilon=1e-5,training=training)
         out = tf.nn.leaky_relu(out)
 
         out = tf.layers.flatten(out)
-        out = tf.layers.dense(out, 1,kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
+        out = tf.layers.dense(out, 1)
         # out = tf.nn.sigmoid(out)
     return out
 
