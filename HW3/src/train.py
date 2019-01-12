8e1bffa87509a2998d4722910de587f579b1d352
CodePlay2016 <13269308268@163.com>
2019-01-12 17:44:07 +0800

    further  finetune

--- HW3/src/train.py
+++ HW3/src/train.py
@@ -4,8 +4,8 @@ import datetime, pdb
 
 d_pretrain_iter = 0
 max_iter = 100000
-d_k_step, g_k_step = 1, 1
-lr_d, lr_g = 1e-4, 1e-4
+d_k_step, g_k_step = 5, 1
+lr_d, lr_g = 5e-5, 5e-5
 show_interval = 100 // ((d_k_step + g_k_step) // 2)
 save_interval = 200
 batch_size = 64 
@@ -18,7 +18,7 @@ clip_value = [-0.01,0.01]
 tf.reset_default_graph()
 image_record = data.readRecord('../data/train_clean.tfrecords')
 train_from_checkpoint = True
-checkpoint_dir = "../model/20190109-090711/"
+checkpoint_dir = "../model/20190110-090118_from_20190109-090711/"
 stddev_scheme = [0] if train_from_checkpoint else [1e-3]#[ii*1e-5 for ii in range(50,0,-1)]+[0] #[0.01,0.009,...,0.001]
 scheme_step = 2000
 
@@ -82,7 +82,11 @@ tf.summary.image('Generated_images', fake_image_show, top_k)
 tf.summary.image('original_images', real_image, top_k)
 time_info = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 logdir = "../tensorboard/" + time_info + "/"
-fine_tune_msg = "_ft_from_"+checkpoint_dir.split('/')[-2] if train_from_checkpoint else ""
+if train_from_checkpoint:
+    pre_data = checkpoint_dir.split('/')[-2].split('_ft_from_')[0]
+    fine_tune_msg = "_ft_from_" + pre_data
+else:
+    fine_tune_msg = ""
 model_path = "../model/" + time_info + fine_tune_msg + "/"
 merged = tf.summary.merge_all()
 ginit = tf.global_variables_initializer()
